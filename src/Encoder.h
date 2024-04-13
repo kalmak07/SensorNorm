3f1b177b90775f18f58098a949aeef69988f7f65
kalmak07 <85942208+kalmak07@users.noreply.github.com>
2024-04-13 18:57:03 +0300

    Add files via upload

---
+++ src/Encoder.h
@@ -0,0 +1,48 @@
+#ifndef Encoder_H
+#define Encoder_H
+
+#include "Arduino.h"
+
+class Enc {
+
+private:
+
+  int pinEnc;
+  long value;
+  bool flagEnc;
+
+public:
+
+  Enc() {}
+
+  Enc(int PinEnc) {
+    pinEnc = PinEnc;
+
+    flagEnc = digitalRead(pinEnc);
+    value = 0;
+  }
+  
+  void tick()
+  {
+    if (flagEnc != digitalRead(pinEnc))
+    {
+      flagEnc = digitalRead(pinEnc);
+      if (!flagEnc){
+        value += 1;
+      }
+    }
+  }
+
+  long getSteps(){
+    return value;
+  }
+
+  float getDegrees(){
+    return ((360.0 / 500.0 ) * value);
+  }
+
+  void resetCount(){
+    value = 0;
+  }
+
+};
